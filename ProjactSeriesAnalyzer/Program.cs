e85b2f47eb1f1264d518f415c7f5a73614dad7bb
Shaul Kimel <shaullk600@gmail.com>
2025-05-12 17:10:16 +0300

    add function "void" validationOfInput - is Empty , and "Array" changingNumbersToArrays - chenge to array

--- ProjactSeriesAnalyzer/Program.cs
+++ ProjactSeriesAnalyzer/Program.cs
@@ -13,12 +13,55 @@ namespace ProjactSeriesAnalyzer
          @param input: null
             @return: a string of numbers
          */
-        static string GetInputNumbers()
+        static string getInputNumbers()
         {
             string input;
             Console.WriteLine("Enter a series of positive numbers: ");
             input = Console.ReadLine();
             return input;
+        }
+
+        /*
+         * a Method that checks if the input is empty or null.
+         @param input: string
+            @return: void
+         */
+        static void validationOfInput(string input)
+        {
+            if (input == "" || input == null)
+            {
+                Console.WriteLine("Input cannot be empty.");
+                return ;
+            }
+        }
+
+        /*
+         * a Method that chenges the string of numbers to array.
+         @param input: string
+            @return: Array of numbers.
+         */
+        static Array changingNumbersToArrays(string input) 
+        {
+            string[] check = input.Split(' ');
+            int[] numbers = new int[check.Length];
+
+            for (int i = 0; i < check.Length; i++)
+            {
+                int num = 0;
+                bool isTypeInt = int.TryParse(check[i], out num);
+                if (isTypeInt)
+                {
+                    numbers[i] = num;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Input must contain only numbers.");
+                    int[] empty = { 000 }; //flag for empty array.
+                    return empty;
+                }
+            }
+            return numbers;
         }
         static void getNumberSeries()
         {
