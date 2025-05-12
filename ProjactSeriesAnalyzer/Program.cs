7aea28e6e3d89b4096346db30df0a6d59095f4b4
Shaul Kimel <shaullk600@gmail.com>
2025-05-12 21:00:51 +0300

    add Method Max()

--- ProjactSeriesAnalyzer/Program.cs
+++ ProjactSeriesAnalyzer/Program.cs
@@ -8,6 +8,25 @@ namespace ProjactSeriesAnalyzer
 {
     internal class Program
     {
+        /*
+         * a Method this calculate value Max of array
+         * @param: array of index Int
+         @return: int - the Max value of Array
+         */
+        static int MethodOfMaxValue(int[] arr)
+        {
+            int numberFlagMax = 0;
+
+            foreach (int i in arr) 
+            {
+                int num = i;
+                if(num > numberFlagMax)
+                {
+                    numberFlagMax = num;
+                }
+            }
+            return numberFlagMax;
+        }
         static void Main(string[] args)
         {
         }
