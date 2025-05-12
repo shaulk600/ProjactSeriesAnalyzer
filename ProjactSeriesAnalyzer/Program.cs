d2ff802bd78969f9f3534bd7b066dbae25b12e62
Shaul Kimel <shaullk600@gmail.com>
2025-05-12 16:00:17 +0300

    Method getInputNumbers that received from the "user"

--- ProjactSeriesAnalyzer/Program.cs
+++ ProjactSeriesAnalyzer/Program.cs
@@ -8,6 +8,22 @@ namespace ProjactSeriesAnalyzer
 {
     internal class Program
     {
+        /*
+         * a Method that takes a string of numbers received from the user.
+         @param input: null
+            @return: a string of numbers
+         */
+        static string GetInputNumbers()
+        {
+            string input;
+            Console.WriteLine("Enter a series of positive numbers: ");
+            input = Console.ReadLine();
+            return input;
+        }
+        static void getNumberSeries()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
