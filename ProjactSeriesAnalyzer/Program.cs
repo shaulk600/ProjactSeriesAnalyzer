6663e49284d0f908b23f521e336c26aa39d8ba07
Shaul Kimel <shaullk600@gmail.com>
2025-05-13 10:26:47 +0300

    add Method "void" display and add Method "int[]" Sort including testing

--- ProjactSeriesAnalyzer/Program.cs
+++ ProjactSeriesAnalyzer/Program.cs
@@ -8,6 +8,39 @@ namespace ProjactSeriesAnalyzer
 {
     internal class Program
     {
+        /*
+         a Method that print of array
+        @param : array
+        @return :  = print of Console.
+         */
+        static void displayArr(int[] arr)
+        {
+            Console.Write("this array :");
+            foreach (int i in arr)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine(' ');
+        }
+        //static void tstSort()
+        //{
+        //    int[]arr = {5,7,3,1};
+        //    int[] a = addArraySort(arr);
+        //}
+        
+        /*
+         a Method that Sorted of array
+        @param : array
+        @return : new array of sorted.
+         */
+        static int[] addArraySort(int[] arr)
+        {
+            int[] arrSort = new int[arr.Length];
+            Array.Copy(arr, arrSort, arr.Length);
+            Array.Sort(arrSort);
+            
+            return arrSort;
+        }
         static void Main(string[] args)
         {
         }
